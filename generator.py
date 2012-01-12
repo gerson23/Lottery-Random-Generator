6187ef785a72bdf2ea49f511bc72a0cf168ec097
Gérson Carlos <gerson.mtg@gmail.com>
2012-01-12 18:06:12 -0200

    Added printing function

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 import sys
 
-
+'''definition of auxiliary functions'''
 def verify_type(game):
     if game == "mega_sena":
         return False
@@ -15,20 +15,27 @@ def verify_type(game):
     else:
         return True
 
+def print_game(numbers):
+    numbers.sort()
+    print "The luck numbers:",
+    for no in numbers:
+        print no,
+    print
+
+'''end definitions'''
 #reads how many different games user wants
 no_game = sys.argv[1]
 #reads which kind of game user wants
 type_game = sys.argv[2]
 
-if verify_type(type_game) == True:
+if verify_type(type_game):
     print "Wrong time of game passed. Lookup the README for usage"
     sys.exit(1)
 
+#loop as many times as passed by user
 for i in range(no_game):
     millions_numbers = locals()[type_game]()
     print "This is your #%d game"%(i+1)
     print_game(millions_numbers)
 
 print "Good luck!"
-
-
